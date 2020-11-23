5330e8e8714429ffb32bc1f2d24371fc60744d37
Gem Xli <x.li.gem@gmail.com>
2020-11-23 16:31:26 +0800

    perf: 区分 hosts 文件位置

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -1,6 +1,7 @@
 import debug from 'debug'
 import fs from 'fs-extra'
+import { hostsPath } from '../const'
 
 export const log = debug('switch-hosts')
 
-export const getHosts = () => fs.readFileSync('/etc/hosts', { encoding: 'utf-8' })
+export const getHosts = () => fs.readFileSync(hostsPath, { encoding: 'utf-8' })
--- src/const.ts
+++ src/const.ts
@@ -1,13 +1,18 @@
 import path from 'path'
 import os from 'os'
 
+export const title = 'Switch Hosts'
+
 export const confDir = path.join(os.homedir(), '.switch-hosts')
 
 export const confPath = path.join(confDir, 'data.json')
 
 export const tempHostsPath = path.join(confDir, 'temp.hosts')
 
-export const title = 'Switch Hosts'
+export const hostsPath =
+  os.platform() === 'win32'
+    ? path.join(path.parse(os.homedir()).root, 'windows/system32/drivers/etc/hosts')
+    : '/etc/hosts'
 
 export enum IPC_EVENTS {
   SAVE_CONFIG = 'save-config',
