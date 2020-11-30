c48239c39f3026812d5bf86f6f0ee263d0d7946e
Gem Xli <x.li.gem@gmail.com>
2020-11-30 19:34:58 +0800

    chore: 添加 debug

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,9 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 const config = {
+  configureWebpack: {
+    devtool: 'source-map'
+  },
   chainWebpack(ctx) {
     ctx.plugin('monaco-editor').use(MonacoWebpackPlugin)
   },
