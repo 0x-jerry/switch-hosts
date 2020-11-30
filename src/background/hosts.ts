8d403a03cb7e0ecc17e751d5cdf5107305fd8d71
Gem Xli <x.li.gem@gmail.com>
2020-11-30 20:33:15 +0800

    perf: 移除 clear chrome dns 相关代码

--- src/background/hosts.ts
+++ src/background/hosts.ts
@@ -3,8 +3,6 @@ import { platform, sysHostsPath, tempHostsPath } from '../const'
 import { log } from './utils'
 import { exec } from 'child_process'
 import { globalStore } from './store'
-// @ts-ignore
-import CDP from 'chrome-remote-interface'
 
 async function changeSysHosts(hosts: string) {
   if (platform === 'win32') {
@@ -25,33 +23,11 @@ async function changeSysHosts(hosts: string) {
   }
 }
 
-async function flushChromeDNSCache() {
-  try {
-    const client = await CDP()
-    const { Runtime } = client
-    await Runtime.enable()
-
-    let r = await Runtime.evaluate({ expression: 'chrome.benchmarking.clearHostResolverCache();' })
-    log('clearHostResolverCache result: %o', r)
-    r = await Runtime.evaluate({ expression: 'chrome.benchmarking.clearCache();' })
-    log('clearCache result: %o', r)
-    r = await Runtime.evaluate({ expression: 'chrome.benchmarking.closeConnections();' })
-    log('closeConnections result: %o', r)
-
-    await Runtime.disable()
-
-    log('Clear chrome DNS cache.')
-  } catch (error) {
-    log('Clear chrome DNS error: %s', error)
-  }
-}
-
 export async function switchHosts(hosts: string) {
   await fs.writeFile(tempHostsPath, hosts)
 
   try {
     await changeSysHosts(hosts)
-    await flushChromeDNSCache()
     log('Save hosts:\n %s', hosts)
   } catch (error) {
     log('Switch host error: \n%s', error)
